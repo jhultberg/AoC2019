6462709f2e2faaa4369ed1198a4f8196976a896d
ezhuljo <johanna.hultberg@ericsson.com>
2019-12-06 07:16:39 +0100

    Added some abstraction for day 5

--- aoc/day5.py
+++ aoc/day5.py
@@ -9,104 +9,131 @@ OP_EQ = "08"
 OP_EXIT = "99"
 
 
-def extract_value(array, instruction):
+class Program:
+    def __init__(self, program):
+         self.ptr = 0
+         self.program = list(program)
+
+    def read(self, immediate=False):
+        value = self.program[self.ptr]
+        self.ptr += 1
+        if not immediate:
+            return value
+        else:
+            return self.program[value]
+
+    def set_pc(self, position):
+        self.ptr = position
+
+    def set(self, idx, value, immediate = False):
+        if not immediate:
+            self.program[idx] = value
+        else:
+            self.program[self.program[idx]] = value
+
+def extract_value(program, instruction):
     if instruction[0] == "0":
-        return array[instruction[1]]
+        return program[instruction[1]]
     if instruction[0] == "1":
         return instruction[1]
 
 
-def mul(array, values):
-    array[values[2]] = values[0] * values[1]
+def mul(program, values):
+    program[values[2]] = values[0] * values[1]
 
 
-def add(array, values):
-    array[values[2]] = values[0] + values[1]
+def add(program, values):
+    program[values[2]] = values[0] + values[1]
 
 
-def write(array, value, input_val):
-    array[value] = input_val
+def write(program, value, input_val):
+    program[value] = input_val
 
 
-def less_than(array, value):
+def less_than(program, value):
     if value[0] < value[1]:
-        array[value[2]] = 1
+        program[value[2]] = 1
     else:
-        array[value[2]] = 0
+        program[value[2]] = 0
 
 
-def equal(array, value):
+def equal(program, value):
     if value[0] == value[1]:
-        array[value[2]] = 1
+        program[value[2]] = 1
     else:
-        array[value[2]] = 0
+        program[value[2]] = 0
+
+def find_data_three_values(program, position, instructions):
+    first_mode = instructions[2]
+    second_mode = instructions[1]
+    first = program[position + 1]
+    second = program[position + 2]
+    third = program[position + 3]
+    return [
+        extract_value(program, (first_mode, first)),
+        extract_value(program, (second_mode, second)),
+        third
+    ]
 
 
-def calc(input_data, input_val):
-    array = list(input_data)
+def find_data_two_values(program, position, instructions):
+    first_mode = instructions[2]
+    second_mode = instructions[1]
+    first = program[position + 1]
+    second = program[position + 2]
+    return [
+        extract_value(program, (first_mode, first)),
+        extract_value(program, (second_mode, second))
+    ]
+
+
+def find_data_one_value(program, position, instructions):
+    first_mode = instructions[2]
+    first = program[position + 1]
+    return [extract_value(program, (first_mode, first))]
+
+
+def calc_diagnostics(input_data, input_val):
+    program = list(input_data)
     position = 0
     output = 0
     while True:
-        instruction = array[position]
+        instruction = program[position]
         instruction = str(instruction).zfill(5)
         opcode = instruction[3:]
-        values = []
-        if opcode == OP_ADD or opcode == OP_MUL or opcode == OP_LT or opcode == OP_EQ:
-            first_mode = instruction[2]
-            second_mode = instruction[1]
-            first = array[position + 1]
-            second = array[position + 2]
-            third = array[position + 3]
-            values = [
-                extract_value(array, (first_mode, first)),
-                extract_value(array, (second_mode, second)),
-                third,
-            ]
-            position += 4
-        elif opcode == OP_INPUT:
-            write(array, array[position + 1], input_val)
-            position += 2
-        elif opcode == OP_OUTPUT:
-            first_mode = instruction[2]
-            first = array[position + 1]
-            values = [extract_value(array, (first_mode, first))]
-            position += 2
-        elif opcode == OP_JUMP_IF_TRUE or opcode == OP_JUMP_IF_FALSE:
-            first_mode = instruction[2]
-            second_mode = instruction[1]
-            first = array[position + 1]
-            second = array[position + 2]
-            values = [
-                extract_value(array, (first_mode, first)),
-                extract_value(array, (second_mode, second)),
-            ]
-        elif opcode == OP_EXIT:
-            return output
-        else:
-            raise ValueError("Unexpected OP-code")
 
         if opcode == OP_ADD:
-            add(array, values)
+            add(program, find_data_three_values(program, position, instruction))
+            position += 4
         elif opcode == OP_MUL:
-            mul(array, values)
+            mul(program, find_data_three_values(program, position, instruction))
+            position += 4
         elif opcode == OP_INPUT:
-            continue
+            write(program, program[position + 1], input_val)
+            position += 2
         elif opcode == OP_OUTPUT:
-            output = values[0]
+            output = find_data_one_value(program, position, instruction)[0]
+            position += 2
         elif opcode == OP_JUMP_IF_TRUE:
+            values = find_data_two_values(program, position, instruction)
             if values[0] != 0:
                 position = values[1]
             else:
                 position += 3
         elif opcode == OP_JUMP_IF_FALSE:
+            values = find_data_two_values(program, position, instruction)
             if values[0] == 0:
                 position = values[1]
             else:
                 position += 3
         elif opcode == OP_LT:
-            less_than(array, values)
+            less_than(program, find_data_three_values(program, position, instruction))
+            position += 4
         elif opcode == OP_EQ:
-            equal(array, values)
+            equal(program, find_data_three_values(program, position, instruction))
+            position += 4
+        elif opcode == OP_EXIT:
+            return output
         else:
             raise ValueError("Unexpected OP-code")
 
@@ -118,6 +145,6 @@ def solve(path):
     for instruction in input:
         data.append(int(instruction))
 
-    a = calc(data, 1)
-    b = calc(data, 5)
+    a = calc_diagnostics(data, 1)
+    b = calc_diagnostics(data, 5)
     return (a, b)
