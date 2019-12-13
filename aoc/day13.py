c01ebfc57e72c029c8fa8e00f477eb9ab95bf84d
ezhuljo <johanna.hultberg@ericsson.com>
2019-12-13 13:17:23 +0100

    Updated rendering of game board for day 13

--- aoc/day13.py
+++ aoc/day13.py
@@ -2,32 +2,42 @@ from .intcode import run
 from collections import defaultdict, deque
 from operator import itemgetter
 from itertools import count
+import time
 
 
 class Game:
     def __init__(self, program):
         self.program = program
         self.board = defaultdict()
-        self.ball = (-1,-1)
-        self.paddle = (-1,-1)
+        self.ball = (-1, -1)
+        self.paddle = (-1, -1)
         self.score = 0
 
     def render(self):
+        time.sleep(0.01)
         max_x = max(self.board, key=itemgetter(0))[0] + 1
         max_y = max(self.board, key=itemgetter(1))[1] + 1
         min_x = min(self.board, key=itemgetter(0))[0]
         min_y = min(self.board, key=itemgetter(1))[1]
         for y in reversed(range(min_y, max_y)):
             for x in range(min_x, max_x):
-                print(self.board[(x, y)], end="")
+                coord = self.board[(x, y)]
+                if coord == 0:
+                    print(" ", end="")
+                if coord == 1:
+                    print("#", end="")
+                if coord == 2:
+                    print("x", end="")
+                if coord == 3:
+                    print("_", end="")
+                if coord == 4:
+                    print("O", end="")
             print()
         print()
 
-
-
     def joystick(self):
         while True:
-            #self.render()
+            # self.render()
             if self.ball[0] == self.paddle[0]:
                 yield 0
             elif self.ball[0] < self.paddle[0]:
@@ -62,14 +72,12 @@ class Game:
                 return (sum(value == 2 for value in self.board.values()), self.score)
 
 
-
-def arcade(program, play_for_free = False):
+def arcade(program, play_for_free=False):
     if play_for_free:
         program[0] = 2
 
     a, b = Game(program).play_game()
     return (a, b)
-
 
 
 def solve(path):
@@ -80,6 +88,6 @@ def solve(path):
         input_data.append(int(instruction))
 
     a = arcade(input_data)[0]
-    b = arcade(input_data, play_for_free = True)[1]
+    b = arcade(input_data, play_for_free=True)[1]
 
     return (a, b)
--- tests/test_day13.py
+++ tests/test_day13.py
@@ -1,4 +1,5 @@
 from aoc.day13 import solve
 
+
 def test_solve():
     assert solve("data/day13.txt") == (412, 20940)
