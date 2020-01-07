34d1ecee9f3e64a96becc792af6bd5c2a4b2ce71
Johanna Hultberg <johanna.chultberg@gmail.com>
2020-01-07 15:58:03 +0100

    Modified day 22

--- aoc/day22.py
+++ aoc/day22.py
@@ -1,33 +1,6 @@
 import re
 
 
-def shuffle(program, deck):
-    for instruction in program:
-        if instruction[0] == "cut":
-            val = instruction[1]
-            deck = deck[val:] + deck[:val]
-        elif instruction[0] == "incr":
-            val = instruction[1]
-            new_deck = [0] * len(deck)
-            for pos, card in enumerate(deck):
-                new_deck[pos * val % len(deck)] = card
-            deck = new_deck
-        elif instruction[0] == "stack":
-            deck.reverse()
-        else:
-            raise ValueError(f"Unexpected instruction in shuffle: {instruction[0]}")
-    return deck
-
-
-def mega_shuffle(program, deck_size, shuffle_times):
-    # Python cannot overflow integers! Will raise memoryError instead
-    deck = list(range(deck_size))
-    for _ in range(shuffle_times):
-        print(deck.index(0))
-        deck = shuffle(program, deck)
-    return deck
-
-
 def parse_input(input):
     parsed_program = []
     for row in input:
@@ -47,18 +20,39 @@ def parse_input(input):
     return parsed_program
 
 
+def k_and_m(program, deck_size):
+    m = 0
+    k = 1
+    for instruction in program:
+        if instruction[0] == "cut":
+            m -= instruction[1]
+        elif instruction[0] == "incr":
+            k *= instruction[1]
+            m *= instruction[1]
+        elif instruction[0] == "stack":
+            k *= -1
+            m = deck_size - m - 1
+        else:
+            raise ValueError(f"Unexpected instruction in shuffle2: {instruction[0]}")
+    k %= deck_size
+    m %= deck_size
+    return k,  m
+
+
+def shuffle(program, deck_size, position):
+    k, m = k_and_m(program, deck_size)
+    return (k * position +m ) % deck_size
+
+
 def solve(path):
     with open(path) as f:
         input = f.readlines()
 
     program = parse_input(input)
 
-    shuffeled = shuffle(program, list(range(10007)))
-    a = shuffeled.index(2019)
+    a = shuffle(program, 10007, 2019)
 
-    mega_deck_size = 119315717514047
-    no_shufflings = 101741582076661
-    # mega_shuffeled = mega_shuffle(program, mega_deck_size, no_shufflings)
-    # b = mega_shuffeled[2020]
+    mega_deck_size = 119_315_717_514_047
+    no_shufflings = 101_741_582_076_661
     b = 0
     return (a, b)
