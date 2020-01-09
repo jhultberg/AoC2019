7456d2e848df72d129a3ce44a57b8ba1bbb5bfed
Johanna Hultberg <johanna.chultberg@gmail.com>
2020-01-09 08:06:44 +0100

    Added solution for day 22

--- aoc/day22.py
+++ aoc/day22.py
@@ -36,42 +36,27 @@ def k_and_m(program, deck_size):
             raise ValueError(f"Unexpected instruction in shuffle2: {instruction[0]}")
     k %= deck_size
     m %= deck_size
-    return k,  m
+    return k, m
 
 
 def shuffle(program, deck_size, position):
     k, m = k_and_m(program, deck_size)
-    return (k * position +m ) % deck_size
+    return (k * position + m) % deck_size
 
 
-def lcg(k, m, mod, x, iterations = 1):
+def lcg(k, m, mod, x, iterations=1):
     if iterations == 1:
         return (k * x + m) % mod
 
     if iterations < 0:
-         k_inv = pow(k, -1, mod)
-         k = k_inv
-         m = -k * m
-         iterations = abs(iterations)
+        k_inv = pow(k, -1, mod)
+        k = k_inv
+        m = -k * m
+        iterations = abs(iterations)
 
     m = (pow(k, iterations, (k - 1) * mod) - 1) // (k - 1) * m
     k = pow(k, iterations, mod)
     return (k * x + m) % mod
-
-
-#def shuffle_b(program, deck_size):
-#    k, m = k_and_m(program, deck_size)
-#    x = 2019
-#    lcg_rand = LcgRandom(k, m, deck_size, x)
-#    lcg_rand.next()
-#    print(lcg_rand.x)
-#    lcg_rand = LcgRandom(k, m, deck_size, lcg_rand.x)
-#    lcg_rand.skip(-1)
-#
-#    lcg_rand = LcgRandom(k, m, deck_size, x)
-#    print(lcg_rand.x)
-
-
 
 
 def solve(path):
@@ -84,12 +69,8 @@ def solve(path):
 
     mega_deck_size = 119_315_717_514_047
     no_shufflings = 101_741_582_076_661
-    b = 0
 
     k, m = k_and_m(program, mega_deck_size)
-    b = lcg(k, m, mega_deck_size, no_shufflings)
-
-    k1, m1 = k_and_m(program, 10007)
-    print(lcg(k1, m1, 10007, 7614, -1))
+    b = lcg(k, m, mega_deck_size, 2020, -no_shufflings)
 
     return (a, b)
